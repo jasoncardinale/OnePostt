cf9f1926c1a6f59ec9126eb3c58bf69a23901633
jc92me <jasoncardinale19@gmail.com>
2018-10-22 19:02:49 -0400

    modal and button changes

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('#fb_select').click(function(){
+    $( "#fb_select" ).toggleClass("fb_selected");
+  });
+
+  $('#tw_select').click(function(){
+    $( "#tw_select" ).toggleClass("tw_selected");
+  });
+
+  $('#ins_select').click(function(){
+    $( "#ins_select" ).toggleClass("ins_selected");
+  });
+
+  $('#pin_select').click(function(){
+    $( "#pin_select" ).toggleClass("pin_selected");
+  });
+
+  $('#tum_select').click(function(){
+    $( "#tum_select" ).toggleClass("tum_selected");
+  });
+});
