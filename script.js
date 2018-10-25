fc4cd7794056bea4c461addbf0c5a5edb2148764
jc92me <jasoncardinale19@gmail.com>
2018-10-25 19:29:59 -0400

    Revert "small changes"
    This reverts commit c78d10bef1fc4bbfc64edc17f0a2169b2021d783.

--- script.js
+++ script.js
@@ -28,42 +28,26 @@ $(document).ready(function() {
 
 function convert() {
   var twitter = $("#tw_select").hasClass("tw_selected");
-  var facebook = $("#fb_select").hasClass("fb_selected");
-  var instagram = $("#ins_select").hasClass("ins_selected");
-  var pinterest = $("#pin_select").hasClass("pin_selected");
-  var tumblr = $("#tum_select").hasClass("tum_selected");
-
   if(twitter) {
     convertToTweet();
-  }
-  if(facebook) {
-    convertToFacebook();
-  }
-  if(instagram) {
-    convertToInstagram();
-  }
-  if(pinterest) {
-    convertToPinterest();
-  }
-  if(tumblr) {
-    convertToTumblr();
   }
 }
 
 function convertToTweet() {
+  var heading = document.getElementById("edit_heading").value;
+  var post = document.getElementById("edit_content").value;
   var current = 0;
   var count = 0;
   var characters = 0;
   var sentences = [];
   var key_words = [];
-  var tag_list = [];
   var arr = [];
   var result = "";
-  var tagResults = "";
   sentences = post.split(".");
   key_words_init = heading.split(" ");
   key_words = [];
   tag_list = tags.split(", ");
+  key_words = heading.split(" ");
 
   for(int i = 0; i < key_words_init.length; i++) {
     for(int j = 0; j < not_key.length; j++) {
@@ -72,7 +56,7 @@ function convertToTweet() {
       }
     }
   }
-  
+
   for(var i = 0; i < sentences.length-1; i++) {
     var words = sentences[i].split(" ");
     for(var j = 0; j < words.length; j++) {
@@ -113,29 +97,8 @@ function convertToTweet() {
       result += arr[i] + ".";
     }
   }
-  for(var i = 0; i < tag_list.length; i++) {
-    tagResults += "#" + tag_list[i] + " ";
-  }
-
   $("#twitterContainer").css("display", "inherit");
   document.getElementById("twHead").innerHTML = heading;
   document.getElementById("twPost").innerHTML = result;
-  document.getElementById("twTags").innerHTML = tagResults;
   console.log(result.length);
-}
-
-function convertToFacebook() {
-
-}
-
-function convertToInstagram() {
-
-}
-
-function convertToPinterest() {
-
-}
-
-function convertToTumblr() {
-
 }
